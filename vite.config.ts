f44f0893d291009d55c40b92b0c17ed2ecd26276
canito8126-ui <canito8126@gmail.com>
2026-05-29 20:41:26 -0600

    map site

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,37 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { inspectAttr } from 'kimi-plugin-inspect-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [inspectAttr(), react()],
+  plugins: [
+    inspectAttr(),
+    react(),
+    Sitemap({
+      hostname: 'https://wildpath.lat',
+      dynamicRoutes: [
+        '/',
+        '/experiencias',
+        '/inspiracion',
+        '/galeria',
+        '/blog',
+        '/testimonios',
+        '/nosotros',
+        '/contacto',
+        '/terminos',
+        '/cancelacion',
+      ],
+      generateRobotsTxt: true,
+      robots: [
+        {
+          userAgent: '*',
+          allow: '/',
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
